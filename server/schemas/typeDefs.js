5e95aabcba3595035ee43bb721edd27a195f2404
Christofer Bond <chrisbond66@gmail.com>
2022-11-30 17:51:30 -0800

    added user routes/model/schema/auth

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,32 @@
 const { gql } = require("apollo-server-express");
 
 // create our typeDefs
-const typeDefs = gql``;
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    friends: [User]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String): User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addFriend(friendId: ID!): User
+  }
+`;
 
 // export the typeDefs
 module.exports = typeDefs;
