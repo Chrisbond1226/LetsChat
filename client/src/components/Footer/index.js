9bf623a3a03e770cd23c99bd8bf28551e5783893
Christofer Bond <chrisbond66@gmail.com>
2022-12-08 20:02:00 -0800

    styling

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,7 +5,9 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;{new Date().getFullYear()} by Chris</div>
+      <div className="container">
+        &copy;{new Date().getFullYear()} by Chris, Scott, Guy, Max and jocelyn
+      </div>
     </footer>
   );
 };
